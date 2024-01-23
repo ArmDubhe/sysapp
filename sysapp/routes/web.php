1ac1bb0e55c8ce40fd325771813686f685f52df2
Armando <armando@anemona.com>
2024-01-22 18:47:45 -0600

    UPD

--- sysapp/routes/web.php
+++ sysapp/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// About
+Route::get('/about', 
+    function(){ return view('about'); }
+)->name('');
